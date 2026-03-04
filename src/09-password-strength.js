a2b2300dccac15a80c34fca43438bfa699412270
Rajan Kumar <rajanchamp98@gmail.com>
2026-03-04 11:54:49 +0530

    solved 09 question

--- src/09-password-strength.js
+++ src/09-password-strength.js
@@ -27,4 +27,43 @@
  */
 export function checkPasswordStrength(password) {
   // Your code here
+
+  //   * The checker evaluates 5 criteria:
+  //  *   1. At least 8 characters long
+  //  *   2. Contains at least one uppercase letter (A-Z)
+  //  *   3. Contains at least one lowercase letter (a-z)
+  //  *   4. Contains at least one number (0-9)
+  //  *   5. Contains at least one special character (!@#$%^&*()_+-=[]{}|;:,.<>?)
+  if (typeof password != "string") return "weak";
+  if (password?.trim()?.length == 0) return "weak";
+ 
+  const criteria1 = password.trim().length >= 8;
+  const criteria2 = password
+    .trim()
+    .split("")
+    .some((val) => val >= "A" && val <= "Z");
+  const criteria3 = password
+    .trim()
+    .split("")
+    .some((val) => val >= "a" && val <= "z");
+  const criteria4 = password
+    .trim()
+    .split("")
+    .some((val) => val >= 0 && val <= 9);
+  const criteria5 = /[!@#$%^&*()_+\-=\[\]{}|;:,.<>?]/.test(password.trim());
+
+  const conditionArray=[criteria1,criteria2,criteria3,criteria4,criteria5]
+
+  const totalTrue=conditionArray.filter(cond=>cond==true).length
+
+
+  if(totalTrue==1){
+    return "weak"
+  }else if(totalTrue==2 ||totalTrue==3 ){
+    return "medium"
+  }else if(totalTrue == 4){
+    return "strong"
+  }else if(totalTrue ==5){
+    return "very strong"
+  }
 }
