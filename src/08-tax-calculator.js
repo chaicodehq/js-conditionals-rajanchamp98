2701ce1c3292bec7c3c81efba59ff869399ff93e
Rajan Kumar <rajanchamp98@gmail.com>
2026-03-04 10:50:00 +0530

    solved 08 question

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,4 +27,21 @@
  */
 export function calculateTax(income) {
   // Your code here
+
+  if(income<=0)return 0;
+
+  if(income<=10000){
+    return 0
+  }else if(income>10000 && income<=30000){
+    const tax=((income-10000)*10)/100
+    return tax
+  }else if(income>30000 && income <=70000){
+     const tax=2000+((income-30000)*20)/100
+     return tax
+  }else if(income>70000){
+    const tax=2000+8000+((income-70000)*30)/100
+    return tax
+  }
 }
+
+console.log(calculateTax(11000))
